36e0111253480ad3b57a9f6b47c2382d84d810d4
Timothy Johnson <tjohnson@scitent.com>
2017-08-10 20:01:01 +0000

    remove pagely plugins

--- wp-content/mu-plugins/pagely-assets/inc/func/pagely_api.php
+++
@@ -1,7 +0,0 @@
-<?php 
-function pagely_api_request($method = 'GET', $uri = '/controller/method', $params = array() )
-{
-    $api = new PagelyApi();
-
-    return $api->apiRequest($method, $uri, $params);
-}
