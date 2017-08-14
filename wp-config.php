a908f17a2a92b60aa5cf1b67589ae283ca976d7a
Timothy Johnson <tjohnson@scitent.com>
2017-08-14 15:52:47 +0000

    forced SSL in wp-config

--- wp-config.php
+++ wp-config.php
@@ -155,7 +155,38 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+    define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+
+
+/** FORCE SSL on Primary Domain **/
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+    // Redirect to https://$primary_domain/ in the Live environment
+    if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+        /** Replace www.example.com with your registered domain name */
+        $primary_domain = 'abnslearning.com';
+    }
+    else {
+        // Redirect to HTTPS on every Pantheon environment.
+        $primary_domain = $_SERVER['HTTP_HOST'];
+    }
+    $base_url = 'https://'. $primary_domain;
+    define('WP_SITEURL', $base_url);
+    define('WP_HOME', $base_url);
+    if ($_SERVER['HTTP_HOST'] != $primary_domain
+        || !isset($_SERVER['HTTP_X_SSL'])
+        || $_SERVER['HTTP_X_SSL'] != 'ON' ) {
+
+        # Name transaction "redirect" in New Relic for improved reporting (optoinal)
+        if (extension_loaded('newrelic')) {
+        newrelic_name_transaction("redirect");
+        }
+
+        header('HTTP/1.0 301 Moved Permanently');
+        header('Location: '. $base_url . $_SERVER['REQUEST_URI']);
+        exit();
+    }
+}
