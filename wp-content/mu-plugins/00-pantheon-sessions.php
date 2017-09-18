65755caf43c31bbf3eab1848582347664a70c4cd
Peter Malcolm <peter.t.malcolm@gmail.com>
2017-09-18 14:43:08 -0400

    create mu-plugins/00-pantheon-sessions.php

---
+++ wp-content/mu-plugins/00-pantheon-sessions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Chain off to Pantheon native sessions plugin as described here:
+ * https://wordpress.org/plugins/wp-native-php-sessions/
+ */
+include 'wp-native-php-sessions/pantheon-sessions.php';
