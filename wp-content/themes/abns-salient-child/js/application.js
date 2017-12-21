f2c42894ad5630d15b0da072a0dc92c317d29ebe
Don Townsend <don@chie.do>
2017-12-21 11:29:58 -0500

    Remove unnecessary javascript

--- wp-content/themes/abns-salient-child/js/application.js
+++ wp-content/themes/abns-salient-child/js/application.js
@@ -3,10 +3,6 @@
 // The below sets up the namespace for this script to use
 var application = application || {};
 
-if ('serviceWorker' in navigator  && themedir) {
-  navigator.serviceWorker.register(themedir + '/js/serviceworker.js', {scope: '/'});
-}
-
 jQuery(document).ready(function() {
   // Scripts. Encapsulated in an anonymous function to avoid conflicts.
   (function($) {
@@ -18,42 +14,18 @@ jQuery(document).ready(function() {
       var clicked = $(ev.target);
       // if user clicked search tool toggle
       if (
-        clicked.is('#search-tool-toggle') ||
-        clicked.parents().is('#search-tool-toggle'))
+        clicked.is('#some-element-id') ||
+        clicked.parents().is('#some-element-id'))
       {
-        showOrHideSearchBar(ev);
-      }
-      // if user clicked mobile menu toggle or mobile menu close
-      if (
-        clicked.is('#mobile-menu-toggle') ||
-        clicked.parents().is('#mobile-menu-toggle') ||
-        clicked.is('#mobile-menu-close') ||
-        clicked.parents().is('#mobile-menu-close')
-      )
-      {
-        toggleMobileNavMenu(ev);
+        doSomething(ev);
       }
     })
 
     /**
      * Toggle search bar on click
      */
-    function showOrHideSearchBar(ev) {
+    function doSomething(ev) {
       ev && ev.preventDefault();
-      var searchBarDropdown = $('#searchform-dropdown');
-      searchBarDropdown.toggleClass('showing');
-    }
-
-    /**
-     * Show / hide mobile nav menu
-     */
-    var $mobileNavContainer = $('#mobile-menu-container');
-    var $body = $('body');
-    //$(document).on('click', '#mobile-menu-toggle, #mobile-menu-close', toggleMobileNavMenu);
-    function toggleMobileNavMenu(ev) {
-      ev && ev.preventDefault();
-      $mobileNavContainer.toggleClass('active');
-      $body.toggleClass('mobile-nav-showing');
     }
 
   })( jQuery ); // End scripts
