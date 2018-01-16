df8e229669ba0cb89ae38c7b1093fb05cd2029dc
Don Townsend <don@chie.do>
2018-01-16 09:57:04 -0500

    Add footer-copyright widget

--- wp-content/themes/abns-salient-child/functions/setup-widgets.php
+++ wp-content/themes/abns-salient-child/functions/setup-widgets.php
@@ -1,19 +1,25 @@
 <?php
+/**
+ * Define additional widgets
+ *
+ * @package WordPress
+ * @subpackage Salient Child theme
+ */
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function laboratory_widgets_init() {
+function abns_extra_widgets_init() {
   register_sidebar( array(
-    'name'          => 'Main Sidebar',
-    'id'            => 'main-sidebar',
-    'description'   => 'Default Template Sidebar',
+    'name'          => 'Footer Copyright Center',
+    'id'            => 'footer-copyright-center',
+    'description'   => 'Footer copyright section - right column',
     'before_widget' => '<section id="%1$s" class="widget %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h2 class="widget-title">',
     'after_title'   => '</h2>',
   ) );
 }
-add_action( 'widgets_init', 'laboratory_widgets_init' );
+add_action( 'widgets_init', 'abns_extra_widgets_init' );
